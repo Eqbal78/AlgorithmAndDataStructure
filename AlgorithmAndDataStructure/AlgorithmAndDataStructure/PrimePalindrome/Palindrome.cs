f576554bf07edc99ba7dfd8463d5c3b2435901a1
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-26 22:26:38 +0530

    print all prime number in range(0-1000) and check Palindome or not

---
+++ AlgorithmAndDataStructure/AlgorithmAndDataStructure/PrimePalindrome/Palindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmAndDataStructure.PrimePalindrome
+{
+    class Palindrome
+    {
+        public static void PrimePalin()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("The prime numbers are : " + i + "\n");
+                        break;
+                    }
+
+                }
+
+                int r, sum = 0, temp;
+                temp = i;
+                while (i > 0)
+                {
+                    r = i % 10;
+                    sum = (sum * 10) + r;
+                    i = i / 10;
+                }
+                if (temp == sum)
+                {
+                    Console.Write("Number is Palindrome." + i+"\n");
+                    break;
+                }
+                else
+                {
+                    Console.Write("Number is not Palindrome" + i+"\n");
+                    break;
+                }
+                
+                
+            }
+
+        }
+
+    }
+}
--- AlgorithmAndDataStructure/AlgorithmAndDataStructure/Program.cs
+++ AlgorithmAndDataStructure/AlgorithmAndDataStructure/Program.cs
@@ -10,7 +10,7 @@ namespace AlgorithmAndDataStructure
             while (true)
             {
                 Console.WriteLine("Choose Options");
-                Console.WriteLine("1.Permutation \n2.Binary Search \n3.Insertion Sort \n4.Bubble Sort \n5.Merge Sort \n6.Anagram Detection \n7.Prime Number \n8.Exit");
+                Console.WriteLine("1.Permutation \n2.Binary Search \n3.Insertion Sort \n4.Bubble Sort \n5.Merge Sort \n6.Anagram Detection \n7.Prime Number \n8.Prime Palindeome \n9.Exit");
                 Console.WriteLine("*************************************************************************");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -41,7 +41,11 @@ namespace AlgorithmAndDataStructure
                     case 7:
                         PrimeNumber.PrimeNumber.Check();
                         break;
+
                     case 8:
+                        PrimePalindrome.Palindrome.PrimePalin();
+                        break;
+                    case 9:
                         Environment.Exit(0);
                         break;
                 }
