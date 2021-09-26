4198b8492ba183b22dce5c3f11118d6693136634
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-26 10:05:10 +0530

    Find Permutation and Combination

---
+++ AlgorithmAndDataStructure/AlgorithmAndDataStructure/Permutation/Permutation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmAndDataStructure.Permutation
+{
+    public class Permutation
+    {
+        /// <summary>
+        /// Static method passes with some parameters
+        /// </summary>
+        /// <param name="str">Name of string</param>
+        /// <param name="startPos">Starting position</param>
+        /// <param name="endPos">End position</param>
+        public static void Permute(string str, int startPos, int endPos)
+        {
+            //check start position is equal to end position return string
+            if (startPos == endPos)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                //Find diffrent combinations
+                for (int i = startPos; i <= endPos; i++)
+                {
+                    str = Swap(str, startPos, i);
+                    Permute(str, startPos + 1, endPos);
+                    str = Swap(str, startPos, i);
+                }
+            }
+        }
+        /// <summary>
+        /// Swapping the string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        public static String Swap(String str, int i, int j)
+        {
+            char temp;
+            char[] charArray = str.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
---
+++ AlgorithmAndDataStructure/AlgorithmAndDataStructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace AlgorithmAndDataStructure.Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Permutation Problem");
+            Console.WriteLine("Enter a string to permute : ");
+            string str = Console.ReadLine();
+            Console.WriteLine("\nPermutation Combinations is:");
+            int startPos = 0, endPos = str.Length - 1;
+            Permutation.Permute(str, startPos, endPos);
+        }
+    }
+}
